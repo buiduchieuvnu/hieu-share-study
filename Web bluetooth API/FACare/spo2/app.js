fe3c85ba249a94cfd1ee6fc98b97e3bd6dbf7f65
buiduchieuvnu <buiduchieuvnu@gmail.com>
2022-05-15 11:32:55 +0700

    add

--- Web bluetooth API/FACare/spo2/app.js
+++ Web bluetooth API/FACare/spo2/app.js
@@ -6,8 +6,20 @@ var App = function () {
 
     this.initPage = function(){
 		$('#hAppName').html(that.AppName);
+        document.title = that.AppName;
         that.log('Startup!');
 	}
+
+    this.log = function (msg) {
+        var datetime = '';
+        var date = new Date();
+        datetime = date.toLocaleString('en-GB');
+        that.LogContent += datetime + ' | ' + msg + `\r\n`;
+        $('#txtLog').val(that.LogContent);
+        var psconsole = $('#txtLog');
+        if (psconsole.length)
+            psconsole.scrollTop(psconsole[0].scrollHeight - psconsole.height());
+    }
 
     this.clearLog = function(){
         that.LogContent = '';
@@ -68,16 +80,51 @@ var App = function () {
             .catch(error => { console.error(error); });
     }
 
-    this.log = function (msg) {
-        var datetime = '';
-        var date = new Date();
-        datetime = date.toLocaleString('en-GB');
-        that.LogContent += datetime + ' | ' + msg + `\r\n`;
-        $('#txtLog').val(that.LogContent);
-        var psconsole = $('#txtLog');
-        if (psconsole.length)
-            psconsole.scrollTop(psconsole[0].scrollHeight - psconsole.height());
+    this.btnTD8255 = function () {
+        that.log('Bắt đầu kết nối thiết bị bluetooth...');
+        const SERVICE_UUID = '00001523-1212-efde-1523-785feabcd123';
+        const DEVICE_NAME = 'TAIDOC TD8255';
+        navigator.bluetooth.requestDevice({
+            filters: [{
+                name: DEVICE_NAME
+            }],
+            optionalServices: [SERVICE_UUID]
+        }).then(device => {
+            device.gatt.connect();
+            return device.gatt.connect();
+        }).then(server => {
+                return server.getPrimaryService(SERVICE_UUID);
+            }).then(service => {
+                console.log(service);
+                that.log('Tên thiết bị: ' + service.device.name + '; ID: ' + service.device.id);
+                that.log('Trạng thái kết nối: ' + service.device.gatt.connected);
+                return service.getCharacteristic('00001524-1212-efde-1523-785feabcd123')
+                .then(result=>{
+                    that.log('Kết quả: ' + result);
+                    console.log(result);
+                })
+
+            }).then(a => {that.log('----------------- END ---------------'); })
+            .catch(error => { 
+                that.log('ERROR | ' + error); });
     }
+
+    this.getListDevice = function() {
+        that.log('> Tìm kiếm các thiết bị Bluetooth gần bạn...');
+        navigator.bluetooth.getDevices()
+        .then(devices => {
+            that.log('  > Tìm thấy ' + devices.length + ' thiết bị Bluetooth');
+            that.log('  > Danh sách: ');
+          for (const device of devices) {
+            that.log('       >' + device.name );
+          }
+        })
+        .catch(error => {
+            that.log('Kết quả: ' + result);
+        });
+      }
+
+
 
     // Events
     $(document).ready(function () {
@@ -94,6 +141,10 @@ var App = function () {
 
         $('.ACTIONS').on('click','#btnGetDevice',function(){
 			that.getListDevice();
+		});
+
+        $('.ACTIONS').on('click','#btnTD8255',function(){
+			that.btnTD8255();
 		});
 
 	});
