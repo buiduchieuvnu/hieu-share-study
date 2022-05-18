be5cb56e4a9ced40621f937c6949c026d2e0fc43
HoaiNam <nguyenhoainam98@vnpt.vn>
2022-05-18 09:46:55 +0700

    update facare.js l2

--- docs/web-bluetooth/facare.js
+++ docs/web-bluetooth/facare.js
@@ -106,7 +106,7 @@ var FACare = function () {
     this.writeSPO2 = function(){
         const SERVICE_UUID = '00001523-1212-efde-1523-785feabcd123';
         const commandCharacteristicUUID = '00001524-1212-efde-1523-785feabcd123'
-        const descriptor = 'time_trigger_setting'
+        const descriptor = '00002902-0000-1000-8000-00805f9b34fb'
         const DEVICE_NAME = 'TAIDOC TD8255';
         that.log('Tìm kiếm thiết bị...');
         navigator.bluetooth.requestDevice({
@@ -133,12 +133,42 @@ var FACare = function () {
         })
           .then(value => {
             that.log(value);
-            let decoder = new TextDecoder('utf-8');
-            that.log('> Characteristic User Description: ' + decoder.decode(value));
+            console.log(value);
+            console.log(value.getUint8());
           })
         .catch(error => { console.error(error); });
     }
 
+    this.getDescriptorSPO2 = function(){
+        const SERVICE_UUID = '00001523-1212-efde-1523-785feabcd123';
+        const commandCharacteristicUUID = '00001524-1212-efde-1523-785feabcd123'
+        const DEVICE_NAME = 'TAIDOC TD8255';
+        that.log('Tìm kiếm thiết bị...');
+        navigator.bluetooth.requestDevice({
+            filters: [{
+                name: DEVICE_NAME
+            }],
+            optionalServices: [SERVICE_UUID]
+        }).then(device => {
+            that.log('Bắt đầu lấy data từ thiết bị SPO2...');
+            device.gatt.connect();
+            return device.gatt.connect();
+        }).then(server => {
+            that.log('getPrimaryService...');
+            return server.getPrimaryService(SERVICE_UUID);
+        }).then(service => {
+            that.log("Getting Characteristic...");
+            return service.getCharacteristic(commandCharacteristicUUID);
+        }).then(characteristic => {
+            that.log("Getting Descriptors...");
+            return characteristic.getDescriptors();
+        }).then(descriptors => {
+            console.log(descriptors);
+            that.log('> Descriptors: ' +
+              descriptors.map(c => c.uuid).join('\n' + ' '.repeat(19)));
+        })
+        .catch(error => { console.error(error); });
+    }
 
     // Events
     $(document).ready(function () {
@@ -151,6 +181,9 @@ var FACare = function () {
 		});
         $('.ACTIONS').on('click','#btnWriteTD8255-SPO2',function(){
 			that.writeSPO2();
+		});
+        $('.ACTIONS').on('click','#btnDescriptorTD8255-SPO2',function(){
+			that.getDescriptorSPO2();
 		});
 	});
 
